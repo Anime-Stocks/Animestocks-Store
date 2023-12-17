1159d9882260df88fa18e00d3ff62a3bddfdddad
KellyCu <your-public-cukellytom@gmail.com>
2023-12-17 16:04:03 +0800

    temp solution

--- server/Routes/routes.js
+++ server/Routes/routes.js
@@ -4,25 +4,28 @@ const router = express.Router();
 const { authenticateUser, addManga, addLightNovel, addMerch, getManga, getLightNovel, getMerch} = require ('../controllers/prodController');
 
 // Logging the user(need a db to store)
-// router.use('/Login', (req, res, next) => {
+// router.use('/authentication', (req, res, next) => {
 //     const username = req.username || 'Unknown User';
 //     console.log(`Incoming Request for /Loginby ${username}: ${req.method} ${req.url}`);
 //     next();
 // });
 
-router.post('/authenticate', authenticateUser);
+router.post('/authenticate', authenticateUser, (req,res) => {
+    console.log("Authentication middleware succeeded");
+    res.status(200).json({ message: "Authorized" });
+});
 
 router.get("/test", (req, res) => {
     res.json({ message: "Hello from server!" });
 })
 
-// router.post('/addManga', addManga);
-// router.post('/addLightNovel', addLightNovel);
-// router.post('/addMerch', addMerch);
+router.post('/addManga', addManga);
+router.post('/addLightNovel', addLightNovel);
+router.post('/addMerch', addMerch);
 
-// router.get('/manga', getManga);
-// router.get('/lightnovel', getLightNovel);
-// router.get('/merch', getMerch);
+router.get('/manga', getManga);
+router.get('/lightnovel', getLightNovel);
+router.get('/merch', getMerch);
 
 
 module.exports = router;
--- server/controllers/prodController.js
+++ server/controllers/prodController.js
@@ -1,19 +1,17 @@
 // CONTROLLER
 const { Admin, Manga, Light, Merch } = require("../Schemas/schemas");
 
-exports.authenticateUser = async (req, res) => {
+exports.authenticateUser = async (req, res, next) => {
     const { username, password } = req.body;
 
     try {
         const admin = await Admin.findOne({ username, password });
         if (admin) {
-            console.log(`Log in successful for username: "${username}" & password: "${password}"`);
-            console.log("Authorized");
-            res.redirect("/addWhat");
+            next();
         } else {
-            console.log(`Log in failed for username: "${username}" & password: "${password}"`);
-            res.status(401).json({ message: "Unauthorized" });
+            console.log(`FAILED log in for username: "${username}" & password: "${password}"`);
             console.log("Unauthorized");
+            res.status(401).json({ message: "Unauthorized" });
         }
     } catch (error) {
         console.error("Error in authentication:", error);
@@ -21,71 +19,71 @@ exports.authenticateUser = async (req, res) => {
     }
 };
 
-// exports.addManga = (req, res) => {
-//     const name = req.body.name;
-//     const cat = req.body.cat;
-//     const link = req.body.link;
-//     const image = req.body.image;
-//     const description = req.body.description;
-//     const price = req.body.price;
+exports.addManga = (req, res) => {
+    const name = req.body.name;
+    const cat = req.body.cat;
+    const link = req.body.link;
+    const image = req.body.image;
+    const description = req.body.description;
+    const price = req.body.price;
 
-//     const newManga = new Manga({ name: name, cat: cat, link: link, image: image, description: description, price: price });
-//     newManga.save();
-//     // this should return to prev route
-// };
+    const newManga = new Manga({ name: name, cat: cat, link: link, image: image, description: description, price: price });
+    newManga.save();
+    // this should return to prev route
+};
 
-// exports.addLightNovel = (req, res) => {
-//     const name = req.body.name;
-//     const cat = req.body.cat;
-//     const link = req.body.link;
-//     const image = req.body.image;
-//     const description = req.body.description;
-//     const price = req.body.price;
+exports.addLightNovel = (req, res) => {
+    const name = req.body.name;
+    const cat = req.body.cat;
+    const link = req.body.link;
+    const image = req.body.image;
+    const description = req.body.description;
+    const price = req.body.price;
 
-//     const newLight = new Light({ name: name, cat: cat, link: link, image: image, description: description, price: price });
-//     newLight.save();
-//     // this should return to prev route
-// };
+    const newLight = new Light({ name: name, cat: cat, link: link, image: image, description: description, price: price });
+    newLight.save();
+    // this should return to prev route
+};
 
-// exports.addMerch = (req, res) => {
-//     const name = req.body.name;
-//     const cat = req.body.cat;
-//     const link = req.body.link;
-//     const image = req.body.image;
-//     const description = req.body.description;
-//     const price = req.body.price;
+exports.addMerch = (req, res) => {
+    const name = req.body.name;
+    const cat = req.body.cat;
+    const link = req.body.link;
+    const image = req.body.image;
+    const description = req.body.description;
+    const price = req.body.price;
 
-//     const newMerch = new Merch({ name: name, cat: cat, link: link, image: image, description: description, price: price });
-//     newMerch.save();
-//     // this should return to prev route
-// };
+    const newMerch = new Merch({ name: name, cat: cat, link: link, image: image, description: description, price: price });
+    newMerch.save();
+    // this should return to prev route
+};
 
-// exports.getManga = async (req, res) => {
-//     try {
-//         const manga = await Manga.find();
-//         res.json(manga);
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
+exports.getManga = async (req, res) => {
+    try {
+        const manga = await Manga.find();
+        res.json(manga);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
 
-// exports.getLightNovel = async (req, res) => {
-//     try {
-//         const lightnovel = await Light.find();
-//         res.json(lightnovel);
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
+exports.getLightNovel = async (req, res) => {
+    try {
+        const lightnovel = await Light.find();
+        res.json(lightnovel);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
 
-// exports.getMerch = async (req, res) => {
-//     try {
-//         const merch = await Merch.find();
-//         res.json(merch);
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
+exports.getMerch = async (req, res) => {
+    try {
+        const merch = await Merch.find();
+        res.json(merch);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
